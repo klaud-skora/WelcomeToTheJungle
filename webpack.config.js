42630407d4ce9ec17e34ddf48b7c8dc6c921c9f0
Klaudia Skóra <klaud.skora@gmail.com>
2020-04-20 00:01:19 +0200

    Add files for githubpages

--- webpack.config.js
+++ webpack.config.js
@@ -4,132 +4,40 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 
-const baseConfig = () => ({
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'scripts_bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  }, 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new webpack.ProvidePlugin({ // inject ES5 modules as global vars
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      Popper: ['popper.js', 'default']
-  }),
-  ],
-});
-
-
-const devConfig = () => ({
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.svg$/,
-        loader: 'svg-inline-loader'
-      },
-      {
-        test: /\.(jpe?g|png)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'assets/',
-              publicPath: 'assets/'
-            }
-          }
-        ]
-      },
-    ],
-  },
-});
-
-const prodConfig = () => ({
-  module: {
-    rules: [
-      {
-        test:/\.(s*)css$/,
-        use:[
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            query: {
-              modules: true, 
-            },
-          }, 
-          'sass-loader',
-        ],
-      },
-    ],
-  }, 
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'styles_bundle_[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
-});
-
-module.exports = (env, argv) => {
-  const modeConfig = argv.mode == 'production' ? prodConfig : devConfig;
-
-  return merge(baseConfig(), modeConfig());
-};
-
-
-// module.exports = {
-//   entry: "./src/index.js",
+// const baseConfig = () => ({
+//   entry: './src/index.js',
 //   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'script.js',
+//     path: path.join(__dirname, 'dist'),
+//     filename: 'scripts_bundle.js',
 //   },
-
 //   module: {
 //     rules: [
-
-//       { test: /\.html$/,
-//         use: [
-//           {
-//           loader: 'html-loader',
-//           options: { minimize: true }
-//           }
-//         ]
-//       },
-     
-//       { test: /\.js$/,
+//       {
+//         test: /\.js$/,
 //         exclude: /node_modules/,
 //         use: {
 //           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env']
-//           } 
-//         }
+//         },
 //       },
+//     ],
+//   }, 
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html',
+//     }),
+//     new webpack.ProvidePlugin({ // inject ES5 modules as global vars
+//       $: 'jquery',
+//       jQuery: 'jquery',
+//       'window.jQuery': 'jquery',
+//       Popper: ['popper.js', 'default']
+//   }),
+//   ],
+// });
+
+
+// const devConfig = () => ({
+//   module: {
+//     rules: [
 //       {
 //         test: /\.s[ac]ss$/i,
 //         use: [
@@ -142,32 +50,115 @@ module.exports = (env, argv) => {
 //         ],
 //       },
 //       {
-//         test: /\.(jpe?g|png|svg)$/,
+//         test: /\.svg$/,
+//         loader: 'svg-inline-loader'
+//       },
+//       {
+//         test: /\.(jpe?g|png)$/,
 //         use: [
 //           {
 //             loader: 'file-loader',
 //             options: {
-//               name: 'daftcode.png',
 //               outputPath: 'assets/',
 //               publicPath: 'assets/'
 //             }
 //           }
 //         ]
 //       },
-//     ]
+//     ],
 //   },
+// });
+
+// const prodConfig = () => ({
+//   module: {
+//     rules: [
+//       {
+//         test:/\.(s*)css$/,
+//         use:[
+//           MiniCssExtractPlugin.loader,
+//           {
+//             loader: 'css-loader',
+//             query: {
+//               modules: true, 
+//             },
+//           }, 
+//           'sass-loader',
+//         ],
+//       },
+      
+//     ],
+//   }, 
 //   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: path.join(__dirname, './src/index.html'),
-//       filename: './index.html'
+//     new MiniCssExtractPlugin({
+//       filename: 'styles_bundle_[name].css',
+//       chunkFilename: '[id].css',
 //     }),
-//     new BrowserSyncPlugin({
-//       host: 'localhost',
-//       port: 3000,
-//       server: { baseDir: ['public'] }
-//     }, {
-//       reload: false,
-//     })
-    
-//   ]
-// }
+//   ],
+// });
+
+// module.exports = (env, argv) => {
+//   const modeConfig = argv.mode == 'production' ? prodConfig : devConfig;
+
+//   return merge(baseConfig(), modeConfig());
+// };
+
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'script.js',
+  },
+
+  module: {
+    rules: [
+
+      { test: /\.html$/,
+        use: [
+          {
+          loader: 'html-loader',
+          options: { minimize: true }
+          }
+        ]
+      },
+     
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          } 
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|svg|gif|ico|webp|eot|ttf|woff|woff2|pdf|mp4|webm)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/icons/',
+            },
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+      filename: './index.html'
+    }),
+  ]
+}
