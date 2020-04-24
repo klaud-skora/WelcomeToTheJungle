7c11b4136652a95cac5c30c077b4d37ead71a145
Klaudia Skóra <klaud.skora@gmail.com>
2020-04-24 14:27:53 +0200

    Make scroller dissappear when it's on the top of the site

--- src/script.js
+++ src/script.js
@@ -1,8 +1,15 @@
-const script = {
+const script = { 
 
   scrollToTop: () => {
+    const height = window.innerHeight;
     const scroller = document.querySelector('.scroller');
-    scroller.addEventListener('click', () => {(window).scrollTo({ top: 0, behavior: 'smooth' }) });
+    
+    window.addEventListener('scroll', () => {
+      let offset = window.pageYOffset;
+      if(offset < height * 0.5 ) { scroller.classList.add('hidden'); }
+      else { scroller.classList.remove('hidden'); }
+    });
+    scroller.addEventListener('click', () => { (window).scrollTo({ top: 0, behavior: 'smooth' }) });
   },
 
   loadProducts: () => {
@@ -22,8 +29,6 @@ const script = {
     document.querySelector('.date').innerHTML = date.getFullYear(); 
   },
     
-  
-
   init: function() {
     this.scrollToTop();
     this.loadProducts();
