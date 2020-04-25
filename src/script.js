8eee43096f9f7d20c4d4b217eac888ba6f0c3a7e
Klaudia Skóra <klaud.skora@gmail.com>
2020-04-25 14:29:25 +0200

    Change carousel for pure js carousel and modify scroller media queries

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ const script = {
   scrollToTop: () => {
     const height = window.innerHeight;
     const scroller = document.querySelector('.scroller');
-    
+
     window.addEventListener('scroll', () => {
       let offset = window.pageYOffset;
       if(offset < height * 0.5 ) { scroller.classList.add('hidden'); }
@@ -24,15 +24,79 @@ const script = {
     });
   },
 
-  getDate: function() { 
+  getDate: () => { 
     const date = new Date();
     document.querySelector('.date').innerHTML = date.getFullYear(); 
+  },
+
+  initCarousel: () => {
+    const width = window.innerWidth;
+
+    const prev = document.querySelector('.button.prev');
+    const next = document.querySelector('.button.next');
+
+    function Carousel() {
+      this.slides = width > 850 ? document.querySelectorAll('.caro') : document.querySelectorAll('.caro .card');
+      this.total = this.slides.length - 1;
+      this.current = 0;
+      
+      this.slide(this.current);
+    }
+    // NEXT
+    Carousel.prototype.next = function (interval) {
+      (this.current === this.total) ? this.current = 0 : this.current += 1;
+      
+      this.slide(this.current);
+      
+      if(typeof interval === 'number' && (interval % 1) === 0) {
+        let context = this;
+        this.run = setTimeout(function() {
+          context.next(interval);
+        }, interval);
+      }
+    };
+    // PREVIOUS
+    Carousel.prototype.prev = function (interval) {	
+      (this.current === 0) ? this.current = this.total : this.current -= 1;
+        
+      this.slide(this.current);
+      
+      if(typeof interval === 'number' && (interval % 1) === 0) {
+        let context = this;
+        this.run = setTimeout(function() {
+          context.prev(interval);
+        }, interval);
+      }
+    };
+    // SELECT SLIDE
+    Carousel.prototype.slide = function (index) {	
+      if (index >= 0 && index <= this.total) { 
+        for (let s = 0; s <= this.total; s++) {
+          if (s === index) {
+            this.slides[s].style.display = "flex"; 
+          } else {
+            this.slides[s].style.display = 'none';
+          }
+        }
+      } 
+    };
+    var elements = new Carousel();
+    elements.next(3000);
+
+    prev.addEventListener('click', () => {
+      elements.prev();
+    });
+
+    next.addEventListener('click', () => {
+      elements.next();
+    });
   },
     
   init: function() {
     this.scrollToTop();
     this.loadProducts();
     this.getDate();
+    this.initCarousel();
   },
 };
 
